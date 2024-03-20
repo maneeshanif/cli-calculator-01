c36a56ce33a0946a4cae304f728b6f58fcb9ee4c
maneeshanif <manees7865@gmail.com>
2024-03-20 20:14:08 +0500

    push my code to github

---
+++ README.md
@@ -0,0 +1,1 @@
+In this package, I made simple cli calculator using inquirer ,chalk and nested else-if 
---
+++ index.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+///////////////////////////////////////////////////////////////////////////// CLI CALCULATOR /////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////    Using Inquirer, Chalk and Nested if else             /////////////////////////////////////////////////////////////
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answer = await inquirer.prompt([
+  { message: "Enter First number", type: "number", name: "firstNumber" },
+  { message: "Enter Second number", type: "number", name: "SecondNumber" },
+  {
+    message: "Select one of the operators to perform operations",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+if (answer.operator === "Addition") {
+  console.log(chalk.redBright.bold.bgBlueBright(answer.firstNumber + answer.SecondNumber));
+} else if (answer.operator === "Subtraction") {
+  console.log(chalk.blueBright.bold.bgGreen(answer.firstNumber - answer.SecondNumber));
+} else if (answer.operator === "Multiplication") {
+  console.log(chalk.redBright.bold.bgWhite(answer.firstNumber * answer.SecondNumber));
+} else if (answer.operator === "Division") {
+  console.log(
+    chalk.green.bold.bgRedBright(answer.firstNumber / answer.SecondNumber)
+  );
+} else {
+  console.log(chalk.redBright.strikethrough.bgBlueBright("Please Select invalid operator"));
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
